b48ea4e8782437dd167c4519541c1bc320ac2561
1998horacio <1998horacio@gmail.com>
2023-05-26 13:44:17 -0600

    PIA

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult CMPNT()
+        {
+            return View();
+        }
+
+        public ActionResult cel()
+        {
+            return View();
+        }
+
+        public ActionResult smart()
+        {
+            return View();
+        }
+
+        public ActionResult lap()
+        {
+            return View();
+        }
+    }
+}
---
+++ Views/Home/JavaScript.js
@@ -0,0 +1,26 @@
+﻿// Código JavaScript para agregar productos al carrito
+var botonesAgregarCarrito = document.querySelectorAll('.agregar-carrito');
+botonesAgregarCarrito.forEach(function (boton) {
+    boton.addEventListener('click', function () {
+        var producto = this.parentElement;
+        var nombre = producto.querySelector('h2').textContent;
+        var descripcion = producto.querySelector('p').textContent;
+        var cantidad = parseInt(producto.querySelector('.cantidad').value);
+
+        // Crea un objeto de producto con los detalles
+        var productoCarrito = {
+            nombre: nombre,
+            descripcion: descripcion,
+            cantidad: cantidad
+        };
+
+        // Agrega el producto al carrito (puedes implementar esta función según tus necesidades)
+        agregarAlCarrito(productoCarrito);
+    });
+});
+
+// Función para agregar el producto al carrito (ejemplo)
+function agregarAlCarrito(producto) {
+    // Implementa tu lógica para agregar el producto al carrito aquí
+    console.log('Producto agregado al carrito:', producto);
+}
